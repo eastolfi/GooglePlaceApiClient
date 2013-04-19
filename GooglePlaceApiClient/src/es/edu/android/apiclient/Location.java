af6462eb448f6cf81271e8eac7c8d5456906ab23
eduardo <astolfi_wotc@hotmail.com>
2013-04-19 13:45:19 +0200

    Añadido cliente GPlaces

--- GooglePlaceApiClient/src/es/edu/android/apiclient/Location.java
+++
@@ -1,6 +0,0 @@
-package es.edu.android.apiclient;
-
-public class Location {
-	public double latitude;
-	public double longitude;
-}
--- GooglePlaceApiClient/src/es/edu/android/googleplaceapiclient/TestActivity.java
+++ GooglePlaceApiClient/src/es/edu/android/googleplaceapiclient/TestActivity.java
@@ -1,39 +1,87 @@
 package es.edu.android.googleplaceapiclient;
 
-import es.edu.android.apiclient.Location;
-import es.edu.android.apiclient.PlaceSearch;
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import es.edu.gplaceapiclient.Location;
+import es.edu.gplaceapiclient.Place;
+import es.edu.gplaceapiclient.PlaceSearch;
 
 public class TestActivity extends Activity {
 	String API_KEY = "AIzaSyDxMJ-QyvCXFtRmhVx7BZyqxZboJ1EDxNU";
-	Button bt;
+	Button btSend;
+	EditText txtSearch;
+	Context ctx;
+	ListView lst;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_test);
 		
-		bt = (Button) findViewById(R.id.button1);
-		bt.setOnClickListener(new OnClickListener() {
+		ctx = this;
+		
+		txtSearch = (EditText) findViewById(R.id.txtSearch);
+		
+		lst = (ListView) findViewById(R.id.lstResults);
+		
+		btSend = (Button) findViewById(R.id.btSend);
+		btSend.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
-				new AsyncTask<String, Void, String>() {
-
-					@Override
-					protected String doInBackground(String... arg0) {
-						PlaceSearch ps = new PlaceSearch(API_KEY, PlaceSearch.FORMAT_JSON);
-						Location loc = new Location();
-						loc.latitude = 40.463666;loc.longitude = -3.635402;
-						ps.searchPlacesByLocation(loc, 500, false);
-						return null;
-					}
-				}.execute();
+				if (txtSearch.getText().toString().isEmpty()) {
+					txtSearch.setError("Introduzca algo para buscar");
+				}
+				else {
+					new AsyncTask<String, Void, ArrayList<String>>() { 
+					ProgressDialog dialog;
+						@Override
+						protected void onPreExecute() {
+							dialog = new ProgressDialog(ctx);
+							String msg = "Obteniendo la lista de sitios cercanos a '" +
+										txtSearch.getText().toString() + "' ...";
+							dialog.setMessage(msg);
+							dialog.setIndeterminate(true);
+							dialog.show();
+						}
+						
+						@Override
+						protected ArrayList<String> doInBackground(String... arg0) {
+							PlaceSearch search = new PlaceSearch(API_KEY, PlaceSearch.FORMAT_JSON);
+							
+							String query = txtSearch.getText().toString();
+							query = query.replace(" ", "+");
+							
+							ArrayList<Place> places = search.searchPlacesByName(query, false);
+							
+							ArrayList<String> placeNames = new ArrayList<String>();
+							for (Place place : places) {
+								placeNames.add(place.getName());
+							}
+							
+							return placeNames;
+						}
+						
+						@Override
+						protected void onPostExecute(ArrayList<String> result) {
+							ArrayAdapter<String> adapter = new ArrayAdapter<String>(ctx, android.R.layout.simple_list_item_1, result);
+							lst.setAdapter(adapter);
+							dialog.dismiss();
+						}
+					}.execute();
+				}
 			}
 		});
 	}
